f797e3ae07d55e53301f0f61d7d3c4ea8264b25a
Carlos Dip <cadudip@hotmail.com>
2021-05-18 11:53:37 -0300

    Implemented basic caching for vocabulary calculation;

--- ImgCategorization.py
+++ ImgCategorization.py
@@ -5,12 +5,11 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 import pathlib
 from sklearn.ensemble import RandomForestClassifier
+import json
 
 NUM_CLUSTERS = 40
-print(pathlib.Path().absolute())
 
-origin_dir2 = r"C:\Users\jpgia\Documents\Projetos\ImgClassifier\Assets\Data_Filtered_Resized"
-destination_dir2 = r"C:\Users\jpgia\Documents\Projetos\ImgClassifier\Assets\new"
+origin_dir2 = os.getcwd() + "\\Assets\\Data_Filtered_Resized" #r"C:\Users\jpgia\Documents\Projetos\ImgClassifier\Assets\Data_Filtered_Resized"
 
 
 def collect_img_names(origin_dir2):
@@ -51,12 +50,26 @@ def transforma_imagens(imagens, vocab):
         X.append(representa(vocab, img).flatten())
     return np.array(X)
 
-
+print("Collecting image names...")
 X,Y = collect_img_names(origin_dir2)
+print("Separating train/test split...")
 X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.15, random_state=0)
-print(X_train)
-vocab = cria_vocabulario(X_train, NUM_CLUSTERS)
+print("Bag of visual words...")
+try:
+    print("Attempting to load vocabulary.")
+    if "cache" not in os.listdir(os.getcwd()):
+        os.mkdir("cache")
+    vocab = np.load(os.getcwd() + "\\cache\\vocab.npy", allow_pickle=True)
+    print("Load successful!")
+except Exception as E:
+    print("Load failed! Recalculating vocabulary and saving result.")
+    vocab = cria_vocabulario(X_train, NUM_CLUSTERS)
+    np.save(os.getcwd() + "\\cache\\vocab.npy", vocab, allow_pickle=True)
+
+print("Extracting features...")
 X_train = transforma_imagens(X_train, vocab)
+
+
 # model = RandomForestClassifier()
 # model.fit(X_train,y_train)
 
